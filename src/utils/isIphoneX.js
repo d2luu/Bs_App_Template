0c040ad51378a88cb66cb74eac1ab4be4b36f1a2
d2luu <luubkk57@gmail.com>
2018-07-30 16:55:20 +0900

    Add eslint for dev phase (#2)

--- src/utils/isIphoneX.js
+++ src/utils/isIphoneX.js
@@ -1,19 +1,18 @@
 import { Dimensions, Platform } from 'react-native';
 
 export function isIphoneX() {
-  let dimention = Dimensions.get('window');
+  const dimention = Dimensions.get('window');
   return (
-    Platform.OS === 'ios' &&
-    !Platform.isPad &&
-    !Platform.isTVOS &&
-    (dimention.height === 812 || dimention.width === 812)
+    Platform.OS === 'ios'
+    && !Platform.isPad
+    && !Platform.isTVOS
+    && (dimention.height === 812 || dimention.width === 812)
   );
 }
 
 export function ifIphoneX(iphoneXStyle, regularStyle) {
   if (isIphoneX()) {
     return iphoneXStyle;
-  } else {
-    return regularStyle;
   }
+  return regularStyle;
 }
