aad0c3835cd75d7e7caa37fc948662ed63e9e4ea
d2luu <luubkk57@gmail.com>
2018-07-30 17:30:07 +0900

    edit .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-    "extends": "airbnb"
+  "ecmaFeatures": {
+    "jsx": true
+  },
+  'extends': 'airbnb',
+  'plugins': [
+    'react',
+    'react-native',
+  ],
+  'rules': {
+    'react/no-did-mount-set-state': 2,
+    'react/no-direct-mutation-state': 2,
+    'react/jsx-uses-vars': 2,
+    'no-undef': 2,
+    'semi': 2,
+    'react/prop-types': 2,
+    'react/jsx-no-bind': 2,
+    'react/jsx-no-duplicate-props': 2,
+  },
 };
